a835eb931ee1f3147563c4b823ca110677ef4244
TahsinCr <TahsinCr@outlook.com>
2023-02-27 00:35:45 +0300

    edit test/edit_test.py
    union function added and tested

--- tests/edit_test.py
+++ tests/edit_test.py
@@ -36,7 +36,7 @@ customer_enumerable.add(new_customer) # Adds the new customer to iterable.
 result = customer_enumerable.first(lambda index, customer: customer.id == 13).toValue._toDict()
 print(result, end="\n"*2)
 #--Result :
-# {'id': 13, 'name': 'John', 'age': 13, 'gender': 'MALE'}
+# {'id': 13, 'name': 'John', 'age': 44, 'gender': 'MALE'}
 
 
 new_customer = Customer(id=4,name="Jack",age=29,gender=MALE)
@@ -48,22 +48,36 @@ print(result2, end="\n"*2)
 # {'id': 4, 'name': 'Jack', 'age': 29, 'gender': 'MALE'}
 
 
-customer_enumerable.delete(customer_enumerable.first(lambda index, customer: customer.id == 13).toKey) # Deletes data with given key value from iterable.
-result3 = customer_enumerable.first(lambda index, customer: customer.id == 13).toValue
-print(result3, end="\n"*2)
+new_customers = [Customer(id=14,name="Jack",age=29,gender=MALE),Customer(id=15,name="Jack",age=29,gender=MALE)]
+customer_enumerable.union(new_customers) # Expands with new customers list.
+result3 = customer_enumerable.toValue
+print([c._toDict() for c in result3], end="\n"*2)
 #--Result :
-# None
+# [
+#     {'id': 1, 'name': 'Ava', 'age': 32, 'gender': 'MALE'}, {'id': 2, 'name': 'Alex', 'age': 19, 'gender': 'MALE'}, {'id': 3, 'name': 'Amelia', 'age': 22, 'gender': 'FEMALE'}, 
+#     {'id': 4, 'name': 'Jack', 'age': 29, 'gender': 'MALE'}, {'id': 5, 'name': 'Eric', 'age': 55, 'gender': 'MALE'}, {'id': 6, 'name': 'Lily', 'age': 12, 'gender': 'FEMALE'}, 
+#     {'id': 7, 'name': 'Jessia', 'age': 32, 'gender': 'MALE'}, {'id': 8, 'name': 'William', 'age': 19, 'gender': 'MALE'}, {'id': 9, 'name': 'Emily', 'age': 22, 'gender': 'FEMALE'}, 
+#     {'id': 10, 'name': 'Mateo', 'age': 43, 'gender': 'MALE'}, {'id': 11, 'name': 'Antony', 'age': 55, 'gender': 'MALE'}, {'id': 12, 'name': 'Mia', 'age': 12, 'gender': 'FEMALE'}, 
+#     {'id': 13, 'name': 'John', 'age': 44, 'gender': 'MALE'}, {'id': 14, 'name': 'Jack', 'age': 29, 'gender': 'MALE'}, {'id': 15, 'name': 'Jack', 'age': 29, 'gender': 'MALE'}
+# ]
 
 
-customer_enumerable.remove(customer_enumerable.first(lambda index, customer: customer.id == 12).toValue) # Deletes given data from iterable.
-result4 = customer_enumerable.first(lambda index, customer: customer.id == 12).toValue
+customer_enumerable.delete(customer_enumerable.first(lambda index, customer: customer.id == 13).toKey) # Deletes data with given key value from iterable.
+result4 = customer_enumerable.first(lambda index, customer: customer.id == 13).toValue
 print(result4, end="\n"*2)
 #--Result :
 # None
 
 
-customer_enumerable.insets(lambda index, customer: customer.age) # If the value is iterable change the values ​​in it as desired.
-result5 = customer_enumerable.toValue
+customer_enumerable.remove(customer_enumerable.first(lambda index, customer: customer.id == 12).toValue) # Deletes given data from iterable.
+result5 = customer_enumerable.first(lambda index, customer: customer.id == 12).toValue
 print(result5, end="\n"*2)
 #--Result :
-# [32, 19, 22, 29, 55, 12, 32, 19, 22, 43, 55]
+# None
+
+
+customer_enumerable.insets(lambda index, customer: customer.age) # If the value is iterable change the values ​​in it as desired.
+result6 = customer_enumerable.toValue
+print(result6, end="\n"*2)
+#--Result :
+# [32, 19, 22, 29, 55, 12, 32, 19, 22, 43, 55, 29, 29]
